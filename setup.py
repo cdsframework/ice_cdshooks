1f6538bcb98668db891adb57e6d72640ad2cb824
Samuel Nicolary <sam@nicolary.org>
2018-05-03 22:01:14 -0700

    reformat

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 
-
 setup(name='ice_cdshooks',
       version='1.0.0',
       description='ICE CDS Hooks service',
@@ -8,4 +7,3 @@ setup(name='ice_cdshooks',
       author_email='ice@hln.com',
       url='https://cdsframework.atlassian.net/wiki',
       packages=['ice_cdshooks'])
-
